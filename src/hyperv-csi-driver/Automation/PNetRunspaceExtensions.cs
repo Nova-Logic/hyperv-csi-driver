37d88f713a2f9db038dd3633b603969c56bc20d0
Oleg Novikov <o.novikov@payps.ru>
2023-09-28 23:50:43 +0200

    adding logging

--- src/hyperv-csi-driver/Automation/PNetRunspaceExtensions.cs
+++ src/hyperv-csi-driver/Automation/PNetRunspaceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Reactive.Linq;
@@ -12,11 +13,9 @@ namespace HypervCsiDriver.Automation;
 public static class PNetRunspaceExtensions
 {
     const string ErrorRecordTypeName = "System.Management.Automation.ErrorRecord";
-    private static ILogger _logger;
-    public static void UseLogger(ILogger logger)
-    {
-        _logger = logger;
-    }
+    private static readonly TraceSource _logger = new TraceSource("PNetRunspaceExtensionsTraceSource");
+
+
     public static IObservable<T> Pipe<T>(this Runspace runspace, Func<Pipeline, IObservable<T>> func)
     {
         return Observable.Using(runspace.CreatePipeline, func);
@@ -75,9 +74,6 @@ public static class PNetRunspaceExtensions
     {
         await foreach (var msg in source)
         {
-            //TODO:implement logging of msg here:
-            if (_logger != null)
-                _logger.LogDebug(msg.ToString());
             
             yield return msg;
             if (msg.BaseObject is ErrorRecord error && terminate(error))
@@ -91,11 +87,10 @@ public static class PNetRunspaceExtensions
         }
     }
 
-    public static async IAsyncEnumerable<PSObject> ThrowOnError(this IAsyncEnumerable<PSObject> source)
+    /*public static async IAsyncEnumerable<PSObject> ThrowOnError(this IAsyncEnumerable<PSObject> source)
     {
         await foreach (var msg in source)
         {
-            
             if (msg.BaseObject is ErrorRecord error)
             {
                 ExceptionDispatchInfo.Capture(error.Exception).Throw();
@@ -112,8 +107,40 @@ public static class PNetRunspaceExtensions
                 yield return msg;
             }
         }
+    }*/
+    public static async IAsyncEnumerable<PSObject> ThrowOnError(this IAsyncEnumerable<PSObject> source)
+    {
+        await foreach (var msg in source)
+        {
+            if (msg.BaseObject is ErrorRecord error)
+            {
+                // Log the error message before throwing
+                _logger.TraceEvent(TraceEventType.Error, 0, $"Error: {error.Exception.Message}");
+                
+                // Rethrow the error
+                ExceptionDispatchInfo.Capture(error.Exception).Throw();
+                yield break;
+            }
+            else if (msg.BaseObject is null && msg.TypeNames.Contains(ErrorRecordTypeName))
+            {
+                Exception ex = (msg as dynamic).Exception;
+                
+                // Log the error message before throwing
+                _logger.TraceEvent(TraceEventType.Error, 0, $"Error: {ex.Message}");
+                
+                // Rethrow the error
+                ExceptionDispatchInfo.Capture(ex).Throw();
+                yield break;
+            }
+            else
+            {
+                // Log a message indicating a non-error message
+                _logger.TraceEvent(TraceEventType.Information, 0, "Non-error message received.");
+                
+                yield return msg;
+            }
+        }
     }
-
     public static async IAsyncEnumerable<PSObject> ThrowOnError(this IAsyncEnumerable<PSObject> source, Predicate<ErrorRecord> terminate)
     {
         await foreach (var msg in source)
